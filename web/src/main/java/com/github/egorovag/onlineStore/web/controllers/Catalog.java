b5ae31e0be8a9cd5c51c9e07e23c1066e03427a2
Alexandr Egorov <ealex5@mail.ru>
2020-11-13 16:12:18 +0200

    fix catalog.jspx

--- web/src/main/java/com/github/egorovag/onlineStore/web/controllers/Catalog.java
+++ web/src/main/java/com/github/egorovag/onlineStore/web/controllers/Catalog.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,7 +24,7 @@ public class Catalog {
     @Autowired
     private ProductService productService;
 
-    @PostMapping("/product")
+    @GetMapping("/product")
     public String getProduct(@RequestParam(value = "product") String product , Model model) {
        List<Product> products = productService.getProductByNameService(product);
         if (products == null || products.isEmpty()) {
